e1f7d65defa9dffe68be120c1c78a57a7e2c9be2
Marc <marc.g.smith@gmail.com>
2016-02-16 12:44:12 +0000

    Cleaning up third party lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
+var bower = require('gulp-bower');
 var clean = require('gulp-clean');
 var cssnano = require('gulp-cssnano');
+var install = require('gulp-install');
+var filter = require('gulp-filter');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
@@ -11,8 +14,9 @@ var fs = require("fs");
 var DEST = 'build/';
 var DIST = 'dist/';
 var SRC = 'src/';
+var LIB = 'lib/';
 
-gulp.task('default', ['compile'], function() {
+gulp.task('default', ['install', 'compile'], function() {
 });
 
 gulp.task('compile', ['minify'], function() {
@@ -33,15 +37,24 @@ gulp.task('minify', function() {
 });
 
 gulp.task('clean', function () {
-    return gulp.src([DEST, DIST], {read: false})
+    return gulp.src([DEST, DIST, LIB], {read: false})
         .pipe(clean());
 });
 
-gulp.task('dist', ['clean', 'default'], function() {
+gulp.task ('install', function() {
+    gulp.src(['./bower.json', './package.json'])
+        .pipe(install());
+
+    return bower()
+        .pipe(filter(['**/*.js']))
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('dist', ['clean', 'compile'], function() {
     return gulp.src([DEST + '/*.js', 'README.md', 'LICENSE.md'])
         .pipe(gulp.dest(DIST))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['install', 'compile'], function() {
     gulp.watch('./src/*', ['compile']);
 });
